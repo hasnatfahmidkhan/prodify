a9cab33dd66b09846ef79303059280ba3843051e
hasnatfahmidkhan <hasnatfahmidkhan@gmail.com>
2025-10-08 10:40:24 +0600

    home page hero section donewith responsive

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import Navbar from "../Components/Navbar/Navbar";
 import { Outlet } from "react-router";
 import Footer from "../Pages/Footer/Footer";
-import Container from "../Components/Container/Container";
 
 const RootLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex-1">
-        <Container>
-          <Outlet />
-        </Container>
+        <Outlet />
       </main>
       <Footer />
     </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import playStoreImg from "../../assets/playsote.png";
 import appStoreImg from "../../assets/appstore.png";
+import heroBgImg from "../../assets/hero.png";
 const Home = () => {
   return (
     <div className="mt-5 md:mt-10">
@@ -25,10 +26,40 @@ const Home = () => {
           <img className="max-w-6" src={playStoreImg} alt="Play Store Logo" />
           Google Play
         </a>
-        <a href="https://www.apple.com/app-store/" target="_blank" className="btn">
+        <a
+          href="https://www.apple.com/app-store/"
+          target="_blank"
+          className="btn"
+        >
           <img className="max-w-6" src={appStoreImg} alt="app store logo" />
           App store
         </a>
+      </div>
+
+      <div className="flex items-center justify-center px-4 md:px-8">
+        <img src={heroBgImg} alt="hero section background image" />
+      </div>
+      <div className="bg-gradient-to-br from-[#632EE3] to-[#9F62F2] text-white space-y-4 p-10 md:p-14">
+        <h2 className="text-center text-3xl md:text-4xl xl:text-5xl font-semibold">
+          Trusted by Millions, Built for You
+        </h2>
+        <div className="flex flex-col md:flex-row items-center justify-around xl:max-w-2/3  mx-auto text-center md:py-8 gap-y-4">
+          <div className="space-y-1 md:space-y-4">
+            <p className="">Total Downloads</p>
+            <h3 className="text-2xl md:text-5xl font-extrabold">29.6M</h3>
+            <p>21% more than last month</p>
+          </div>
+          <div className="space-y-4">
+            <p className="">Total Downloads</p>
+            <h3 className="text-2xl md:text-5xl font-extrabold">29.6M</h3>
+            <p>21% more than last month</p>
+          </div>
+          <div className="space-y-4">
+            <p className="">Total Downloads</p>
+            <h3 className="text-2xl md:text-5xl font-extrabold">29.6M</h3>
+            <p>21% more than last month</p>
+          </div>
+        </div>
       </div>
     </div>
   );
